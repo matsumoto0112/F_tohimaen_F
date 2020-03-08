0f2f0038f383e6ed4b35c12c47c78868a1c8eb9d
matsumoto <matsumoto.k017a1377@gmail.com>
2020-03-08 20:52:04 +0900

    プレイヤーのクロスヘアを追加

--- Invisible/Source/Invisible/Player/PlayerHUD.cpp
+++ Invisible/Source/Invisible/Player/PlayerHUD.cpp
@@ -11,5 +11,11 @@ void APlayerHUD::DrawHUD()
 	if (!crossHairTexture)
 		return;
 
+	//キャンバスの中心座標からクロスヘア画像の配置座標を決定する
 	const FVector2D center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
+	const FVector2D crossHairPosition = center - FVector2D(crossHairTexture->GetSurfaceWidth() * 0.5f, crossHairTexture->GetSurfaceHeight() * 0.5f);
+
+	FCanvasTileItem tileItem(crossHairPosition, crossHairTexture->Resource, FLinearColor::White);
+	tileItem.BlendMode = SE_BLEND_Translucent;
+	Canvas->DrawItem(tileItem);
 }
