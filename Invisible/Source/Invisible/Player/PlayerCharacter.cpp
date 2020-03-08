9fc3e4fbb0d84dc5e1abbab0979f7f81d02fb5ad
matsumoto <matsumoto.k017a1377@gmail.com>
2020-03-08 20:17:53 +0900

    コーディング規約の変更に対応

--- Invisible/Source/Invisible/Player/PlayerCharacter.cpp
+++ Invisible/Source/Invisible/Player/PlayerCharacter.cpp
@@ -1,75 +1,71 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "PlayerCharacter.h"
-#include "Engine.h"
 #include "Camera/CameraComponent.h"
+#include "Engine.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
-// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-    //カメラを追加する
-    mCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPSCamera"));
-    mCameraComponent->SetupAttachment(GetCapsuleComponent());
-    //ポーンがカメラの回転を制御できるように
-    mCameraComponent->bUsePawnControlRotation = true;
+	//カメラを追加する
+	cameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FPSCamera"));
+	cameraComponent->SetupAttachment(GetCapsuleComponent());
+	//ポーンがカメラの回転を制御できるように
+	cameraComponent->bUsePawnControlRotation = true;
 }
 
 // Called when the game starts or when spawned
 void APlayerCharacter::BeginPlay()
 {
-    Super::BeginPlay();
-    GetCharacterMovement()->MaxWalkSpeed = mMaxMoveSpeed;
-
+	Super::BeginPlay();
+	GetCharacterMovement()->MaxWalkSpeed = maxMoveSpeed;
 }
 
 // Called every frame
 void APlayerCharacter::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
-
+	Super::Tick(DeltaTime);
 }
 
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-    Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-    //入力にバインドさせる
-    PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::moveForward);
-    PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::moveRight);
-    PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::turn);
-    PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::lookup);
+	//入力にバインドさせる
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::moveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::moveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::lookup);
 }
 
 //前方向への移動
 void APlayerCharacter::moveForward(float value)
 {
-    FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
-    AddMovementInput(direction, value);
+	const FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
+	AddMovementInput(direction, value);
 }
 
 //右方向への移動
 void APlayerCharacter::moveRight(float value)
 {
-    FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
-    AddMovementInput(direction, value);
+	const FVector direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
+	AddMovementInput(direction, value);
 }
 
 //カメラの横方向の回転処理
 void APlayerCharacter::turn(float amount)
 {
-    const float yawValue = mMouseSensitivity * amount * GetWorld()->GetDeltaSeconds();
-    AddControllerYawInput(yawValue);
+	const float yawValue = mouseSensitivity * amount * GetWorld()->GetDeltaSeconds();
+	AddControllerYawInput(yawValue);
 }
 
 //カメラの上下方向の回転処理
 void APlayerCharacter::lookup(float amount)
 {
-    const float pitchValue = mMouseSensitivity * amount * GetWorld()->GetDeltaSeconds();
-    AddControllerPitchInput(pitchValue);
+	const float pitchValue = mouseSensitivity * amount * GetWorld()->GetDeltaSeconds();
+	AddControllerPitchInput(pitchValue);
 }
-
--- Invisible/Source/Invisible/Player/PlayerCharacter.h
+++ Invisible/Source/Invisible/Player/PlayerCharacter.h
@@ -11,49 +11,50 @@ class UCameraComponent;
 UCLASS()
 class INVISIBLE_API APlayerCharacter : public ACharacter
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 public:
-    // Sets default values for this character's properties
-    APlayerCharacter();
+	// Sets default values for this character's properties
+	APlayerCharacter();
 
 protected:
-    // Called when the game starts or when spawned
-    virtual void BeginPlay() override;
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 public:
-    // Called every frame
-    virtual void Tick(float DeltaTime) override;
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
-    // Called to bind functionality to input
-    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
-    /**
+	/**
     * 前方向への移動処理
     */
-    UFUNCTION()
-    void moveForward(float value);
-    /**
+	UFUNCTION()
+	void moveForward(float value);
+	/**
     * 右方向への移動処理
     */
-    UFUNCTION()
-    void moveRight(float value);
-    /**
+	UFUNCTION()
+	void moveRight(float value);
+	/**
     * カメラの横方向の回転処理
     */
-    UFUNCTION()
-    void turn(float amount);
-    /**
+	UFUNCTION()
+	void turn(float amount);
+	/**
     * カメラの上下方向の回転処理
     */
-    UFUNCTION()
-    void lookup(float amount);
+	UFUNCTION()
+	void lookup(float amount);
+
 private:
-    UPROPERTY(EditAnywhere)
-        float mMaxMoveSpeed = 500.0f; //!< 最大移動速度
-    UPROPERTY(EditAnywhere)
-        float mMouseSensitivity = 100.0f; //!< マウス感度
-    UPROPERTY(VisibleAnywhere)
-        UCameraComponent* mCameraComponent; //!< プレイヤーカメラ
+	UPROPERTY(EditAnywhere)
+	float maxMoveSpeed = 500.0f; //!< 最大移動速度
+	UPROPERTY(EditAnywhere)
+	float mouseSensitivity = 100.0f; //!< マウス感度
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* cameraComponent; //!< プレイヤーカメラ
 };
